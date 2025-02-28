a45753b493d01e00e5e6f77bca5000fd8dbc2359
Joshua <joshgonzalez1191@berkeley.edu>
2025-02-28 13:54:35 -0800

    add sys args

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-from stats import get_num_words
-from stats import create_word_count_dict
-from stats import sort_character_dict
+import sys
 
-def get_book_text(path:str) -> str:
+from stats import create_word_count_dict, get_num_words, sort_character_dict
+
+
+def get_book_text(path: str) -> str:
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
+
+def run_sys_command() -> str:
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return sys.argv[1]
+
+
 def main():
-    frankenstein_contents = get_book_text("books/frankenstein.txt")
-    total_words = get_num_words(frankenstein_contents)
-    total_characters = create_word_count_dict(frankenstein_contents)
+    path_to_book = run_sys_command()
+    book_contents = get_book_text(path_to_book)
+    total_words = get_num_words(book_contents)
+    total_characters = create_word_count_dict(book_contents)
     sorted_characters = sort_character_dict(total_characters)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print(f'Found {total_words} total words')
+    print(f"Analyzing book: {path_to_book.split("/")[1].strip("txt")}")
+    print(f"Found {total_words} total words")
     print("--------- Character Count -------")
     for char_dicts in sorted_characters:
         item = list(char_dicts.items())[0]
         if not item[0].isalpha():
             continue
-        print(f'{item[0]}: {item[1]}')
+        print(f"{item[0]}: {item[1]}")
     print("============= END ===============")
+
+
 if __name__ == "__main__":
     main()
-
--- stats.py
+++ stats.py
@@ -1,18 +1,16 @@
 def get_num_words(book: str) -> int:
-    return len(book.split()) 
+    return len(book.split())
+
 
 def create_word_count_dict(book: str) -> int:
     lower_case_book = list(book.lower())
     characters = set(lower_case_book)
-    return {char:lower_case_book.count(char) for char in characters}
+    return {char: lower_case_book.count(char) for char in characters}
 
 
 def sort_character_dict(characters: dict) -> list[dict]:
-    char_dicts = [{char:count} for char, count in zip(characters.keys(), characters.values())]
+    char_dicts = [
+        {char: count} for char, count in zip(characters.keys(), characters.values())
+    ]
     char_dicts.sort(reverse=True, key=lambda d: list(d.values())[0])
     return char_dicts
-    
-
-    
-
-
